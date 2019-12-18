f8c85c76f19cf263ec4e3fc2fa41dadd3ce22aa1
tpiros <2042718+tpiros@users.noreply.github.com>
2019-12-18 17:05:43 +0100

    list all data

---
+++ api/getAll.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const faunadb = require('faunadb');
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB
+});
+
+module.exports = async (req, res) => {
+  try {
+    const queryResponse = await client.query(
+      q.Paginate(
+        q.Match(
+          q.Index('all_cbadge')
+        )
+      )
+    );
+    const badges = queryResponse.data;
+    const allBadgesQuery = badges.map(ref => q.Get(ref));
+
+    const response = await client.query(allBadgesQuery)
+    return res.json(response);
+  } catch(error) {
+    console.error(error);
+    return res.json({
+      body: {
+        error
+      }
+    });
+  }
+  
+};
